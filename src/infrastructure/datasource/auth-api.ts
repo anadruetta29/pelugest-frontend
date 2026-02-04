bd969aedebf4930045fe1d1e5fe85e921c82e58c
anadruetta29 <anadruetta2023@gmail.com>
2026-02-04 17:11:33 -0300

    fix register route

--- src/infrastructure/datasource/auth-api.ts
+++ src/infrastructure/datasource/auth-api.ts
@@ -27,7 +27,7 @@ export class AuthApiDataSource implements AuthDataSourceI {
 
     public async login(dto: LoginUserReq): Promise<LoginUserRes> {
         try {
-            const response = await this.httpClient.post("/api/auth/login", { ...dto });
+            const response = await this.httpClient.post(`/api/auth/login`, { ...dto });
 
             if (response.error) {
                 throw ErrorHandler.handleError(response.error);
@@ -47,7 +47,7 @@ export class AuthApiDataSource implements AuthDataSourceI {
 
     public async register(dto: RegisterUserReq): Promise<void> {
         try {
-            const response = await this.httpClient.post("/api/auth/register", { ...dto });
+            const response = await this.httpClient.post(`/api/auth/register`, { ...dto });
 
             if (response.error) {
                 throw ErrorHandler.handleError(response.error);
