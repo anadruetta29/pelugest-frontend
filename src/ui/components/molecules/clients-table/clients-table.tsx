423b9458dfbe0c7b62c7aaef198a15b2d2f75e37
anadruetta29 <anadruetta2023@gmail.com>
2026-02-03 21:15:40 -0300

    add: generic table component

--- src/ui/components/molecules/clients-table/clients-table.tsx
+++ src/ui/components/molecules/clients-table/clients-table.tsx
@@ -1,58 +1,64 @@
 import type { Client } from "../../../../domain";
 import NoResults from "../../atoms/no-results/no-results";
 import SecondaryButton from "../../atoms/secondary-button/secondary-button";
+import Table from "../../atoms/table/table";
 import style from "./style.module.css";
 
 type Props = {
     clients: Client[] | undefined;
     onEditClient: (client: Client) => void;
     onDeleteClient: (client: Client) => void;
-}
+};
 
-export default function ClientsTable({ clients, onEditClient, onDeleteClient }: Props) {
+export default function ClientsTable({
+    clients,
+    onEditClient,
+    onDeleteClient,
+}: Props) {
     if (!clients || clients.length === 0) {
-        return <NoResults message="No se encontraron clientes" />;
+      return <NoResults message="No se encontraron clientes" />;
     }
 
-    return (
-        <table className={style.table}>
-            <thead>
-                <tr>
-                    <th className={style.tableHeader}>Nombre</th>
-                    <th className={style.tableHeader}>Apellido</th>
-                    <th className={style.tableHeader}>Celular</th>
-                    <th className={style.tableHeader}>Teléfono</th>
-                    <th className={style.tableHeader}>Estado</th>
-                    <th className={style.tableHeader}>Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                {clients.map((client) => (
-                    <tr key={client.id}>
-                        <td className={style.tableContent}>{client.name}</td>
-                        <td className={style.tableContent}>{client.surname}</td>
-                        <td className={style.tableContent}>{client.mobilePhoneNumber}</td>
-                        <td className={style.tableContent}>{client.landlinePhoneNumber}</td>
-                        <td className={style.tableContent}>{client.status.name}</td>
-                        <td className={style.actions}>
-                            <SecondaryButton
-                                enabled
-                                text="Modificar"
-                                type="button"
-                                modifier={style.actionButtons}
-                                onClick={() => onEditClient(client)}
-                            />
-                            <SecondaryButton
-                                enabled
-                                text="Desactivar"
-                                type="button"
-                                modifier={style.actionButtons}
-                                onClick={() => onDeleteClient(client)}
-                            />
-                        </td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    );
+	return (
+		<Table
+			headers={[
+				"Nombre",
+				"Apellido",
+				"Celular",
+				"Teléfono",
+				"Estado",
+				"Acciones",
+			]}
+		>
+			{clients.map((client) => (
+				<tr key={client.id}>
+					<td className={style.tableContent}>{client.name}</td>
+					<td className={style.tableContent}>{client.surname}</td>
+					<td className={style.tableContent}>
+						{client.mobilePhoneNumber}
+					</td>
+					<td className={style.tableContent}>
+						{client.landlinePhoneNumber}
+					</td>
+					<td className={style.tableContent}>{client.status.name}</td>
+					<td className={style.actions}>
+						<SecondaryButton
+						enabled
+						text="Modificar"
+						type="button"
+						modifier={style.actionButtons}
+						onClick={() => onEditClient(client)}
+						/>
+						<SecondaryButton
+						enabled
+						text="Desactivar"
+						type="button"
+						modifier={style.actionButtons}
+						onClick={() => onDeleteClient(client)}
+						/>
+					</td>
+				</tr>
+			))}
+		</Table>
+	);
 }
